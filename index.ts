698641019788b97c75072cb1ac6de24f9ea86ee4
David Vargas <dvargas92495@gmail.com>
2020-10-25 14:42:55 -0400

    chalk and validate

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Creating app...", process.argv[2]);
+#!/usr/bin/env node
+import validateNpmName from 'validate-npm-package-name';
+import chalk from 'chalk';
+
+const projectName = process.argv[2];
+console.log("Creating app...", projectName);
+
+const { validForNewPackages, errors, warnings  } = validateNpmName(projectName)
+if (!validForNewPackages) {
+  console.error(
+    `Could not create a project called ${chalk.red(
+      `"${projectName}"`
+    )} because of npm naming restrictions:`
+  )
+
+  errors?.forEach((p) => console.error(`    ${chalk.red.bold('*')} ${p}`))
+  warnings?.forEach((p) => console.warn(`    ${chalk.yellow.bold('*')} ${p}`))
+  process.exit(1)
+}
